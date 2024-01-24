cf68706ebf0d628238acb13ab478c29626fc92ac
Thinagar <thinagarsivadas@gmail.com>
2024-01-24 20:45:17 +0800

    working

---
+++ data_pipeline/data_pipeline/assets/drupal/drupal_st.py
@@ -0,0 +1,57 @@
+import warnings
+
+import pandas as pd
+from dagster import (
+    AssetExecutionContext,
+    AutoMaterializePolicy,
+    AutoMaterializeRule,
+    ExperimentalWarning,
+    FreshnessPolicy,
+    MaterializeResult,
+    MetadataValue,
+    asset,
+)
+
+warnings.filterwarnings("ignore", category=ExperimentalWarning)
+
+
+@asset(
+    compute_kind="JDBC",
+    # auto_materialize_policy=AutoMaterializePolicy.eager().with_rules(
+    #     AutoMaterializeRule.materialize_on_cron(
+    #         cron_schedule="53 20 * * *", timezone="Asia/Singapore"
+    #     )
+    # ),
+)
+def drupal_st(context: AssetExecutionContext) -> None:
+    print("getting drupal data")
+    context.log.info("getting drupal")
+    print("drupal data retrieved")
+    context.log.info("drupal retrieved")
+
+
+@asset(
+    deps=[drupal_st],
+    metadata={"owner": "Abdul"},
+    compute_kind="pyspark",
+    freshness_policy=(
+        FreshnessPolicy(
+            maximum_lag_minutes=1,
+            cron_schedule="03 12 * * *",
+            cron_schedule_timezone="Asia/Singapore",
+        )
+    ),
+    auto_materialize_policy=AutoMaterializePolicy.eager(),
+)
+def drupal_st_silver(context: AssetExecutionContext) -> None:
+    print("getting drupal_silver")
+    context.log.info("getting drupal_silver")
+    df = pd.DataFrame([[1, 2]], columns=["col_a", "col_b"])
+    print("drupal_silver retrieved")
+    context.log.info("drupal_silver retrieved")
+    return MaterializeResult(
+        metadata={
+            "num_records": len(df),
+            "preview": MetadataValue.md(df.head().to_markdown()),
+        }
+    )
---
+++ data_pipeline/data_pipeline/assets/ga/assets.py
@@ -0,0 +1,65 @@
+from dagster import (
+    AssetExecutionContext,
+    AutoMaterializePolicy,
+    AutoMaterializeRule,
+    BackfillPolicy,
+    DailyPartitionsDefinition,
+    MultiPartitionKey,
+    MultiPartitionsDefinition,
+    StaticPartitionsDefinition,
+    asset,
+)
+
+StaticPartitionsDefinition(["red", "yellow", "blue"])
+
+
+@asset(
+    compute_kind="JDBC",
+    partitions_def=MultiPartitionsDefinition(
+        {
+            "date": DailyPartitionsDefinition(
+                start_date="2024-01-17", timezone="Asia/Singapore"
+            ),
+            "publication": StaticPartitionsDefinition(["st", "bt", "zb"]),
+        }
+    ),
+    backfill_policy=BackfillPolicy.multi_run(max_partitions_per_run=2),
+    # auto_materialize_policy=AutoMaterializePolicy.eager(
+    #     max_materializations_per_minute=10
+    # ).with_rules(
+    #     AutoMaterializeRule.materialize_on_cron(
+    #         cron_schedule="41 22 * * *", timezone="Asia/Singapore"
+    #     )
+    # ),
+)
+def ga(context: AssetExecutionContext) -> None:
+    context.log.info(f"{context.partition_keys}")
+    if isinstance(context.partition_keys[0], MultiPartitionKey):
+        context.log.info(context.partition_keys[0].keys_by_dimension)
+    context.log.info("getting ga data")
+    print("ga data retrieved")
+    context.log.info("ga data retrieved")
+
+
+@asset(
+    deps=[ga],
+    partitions_def=MultiPartitionsDefinition(
+        {
+            "date": DailyPartitionsDefinition(
+                start_date="2024-01-17", timezone="Asia/Singapore"
+            ),
+            "publication": StaticPartitionsDefinition(["st", "bt", "zb"]),
+        }
+    ),
+    compute_kind="pyspark",
+    backfill_policy=BackfillPolicy.multi_run(max_partitions_per_run=2),
+    auto_materialize_policy=AutoMaterializePolicy.eager(
+        max_materializations_per_minute=10
+    ),
+)
+def ga_hits(context: AssetExecutionContext) -> None:
+    print("getting ga hits")
+    context.log.info(f"{context.partition_keys}")
+    context.log.info("getting ga hits")
+    print("ga hits retrieved")
+    context.log.info("ga hits retrieved")
